fdfe4e7bd80c1afdea84cd45f93dd1b0e173c34d
Sumitesh Naithani <sumiteshn@gmail.com>
2023-04-11 01:44:18 +0530

    Added env files in vercel

--- constants/config.js
+++ constants/config.js
@@ -1,14 +1,17 @@
 // Import the functions you need from the SDKs you need
 import * as admin from 'firebase-admin';
-import serviceAccountKey from '../constants/serviceAccountKey.json';
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+//console.log('pk = ', process.env.FIREBASE_PRIVATE_KEY);
+const serviceAccountKey = {
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: process.env.FIREBASE_CERT_URL,
 };
 
 // Initialize Firebase
--- pages/api/getCatalog.js
+++ pages/api/getCatalog.js
@@ -1,11 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { db } from '../../constants/config';
-import { collection, getDocs } from 'firebase/firestore/lite';
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const productsCol = collection(db, 'ondcCatalog');
-    const productSnapshot = await getDocs(productsCol);
-    const productList = productSnapshot.docs.map((doc) => doc.data());
-    res.status(200).json({products: productList});
-  } else res.status(405).json({"msg": `Invalid Request Method. Expected GET got ${req.method}`});
+    const collectionRef = db.collection('ondcCatalog');
+    try {
+      const snapshot = await collectionRef.get();
+      const documents = snapshot.docs.map((doc) => doc.data());
+      console.log('Documents data:', documents);
+      res.status(200).json({ data: documents });
+    } catch (error) {
+      console.error('Error getting documents:', error);
+      res.status(400).json({ msg: 'Unable to get data' });
+    }
+    res.status(200).json({ products: productList });
+  } else
+    res
+      .status(405)
+      .json({ msg: `Invalid Request Method. Expected GET got ${req.method}` });
 }
